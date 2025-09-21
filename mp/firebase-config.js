76a97a786469e99e35f460ff568d9a36489fca21
rlopiccolo <rlopiccolo@gmail.com>
2025-09-21 08:11:49 -0400

    Update firebase-config.js

--- mp/firebase-config.js
+++ mp/firebase-config.js
@@ -2,9 +2,9 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAbKb1NkyBxEHdNreEyd48cZEQaSpLuYCs",
   authDomain: "time-zones-game.firebaseapp.com",
-  databaseURL: "https://time-zones-game-default-rtdb.firebaseio.com", // <- paste EXACTLY from Console
+  databaseURL: "https://time-zones-game-default-rtdb.firebaseio.com",
   projectId: "time-zones-game",
-  storageBucket: "time-zones-game.appspot.com", // not used by our code; ok if different
+  storageBucket: "time-zones-game.appspot.com",
   messagingSenderId: "304047161756",
   appId: "1:304047161756:web:599c1ed492d02d0592c859"
 };
